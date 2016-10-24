c02d2c2b48cc466a3e236a7c84cb0604607f7500
Eliseeva Vika <eliseeva-vika@mail.ru>
2016-10-24 08:20:30 +0300

    second commit

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -10,16 +10,20 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Increase(5));
-            Console.WriteLine(IncreaseOrDecrease(2, 10));
-            Console.WriteLine(IncreaseOrDecrease(10, 3));
-            Console.WriteLine(PowerOfTwo(12));
-            Console.WriteLine(AmountInFigures(1111111));
-            Console.WriteLine(PermutationOfDigits(495039));
-            Console.WriteLine(CheckEase(269,5));
+            Console.WriteLine("From 1 to n: " + Increase(5));
+            Console.WriteLine("From a to b : "+ IncreaseOrDecrease(2, 10));
+            Console.WriteLine("From b to a : "+ IncreaseOrDecrease(10, 3));
+            Console.WriteLine("Power 2 number 12: " +PowerOfTwo(12));
+            Console.WriteLine("Amount in figures -1111111: "+AmountInFigures(1111111));
+            Console.WriteLine("Digit numbers from left to right - 4 9 5 0 3 9 :"+PermutationOfDigits(495039));
+            Console.WriteLine("Verification of the simplicity- 18, 5 : " + (CheckEase(18, 5)));
+            Console.WriteLine("Verification of the simplicity- 29, 5 : " + (CheckEase(29, 5)));
+            
 
             Console.ReadLine();
         }
+
+     
         //1-задача От 1 до n
         public static string Increase(int n)
         {
@@ -85,8 +89,8 @@ namespace ConsoleApplication2
                 PowerOfTwo(n / 2);
                 return "Yes"; // как здесь сдалать так, чтобы сначала полностью рекурсия прошла, и только потом сказал,что Yes
             }
-            
-        
+
+
             else
             {
                 if (n == 1)
@@ -102,106 +106,151 @@ namespace ConsoleApplication2
         public static int AmountInFigures(int n)
         {
             int sum;
-            
-                if (n< 10)
-                {
-                    return sum=+n;
-                }
-            sum=+AmountInFigures(n / 10) + n % 10;
-                return sum;
+
+            if (n < 10)
+            {
+                return sum = +n;
             }
+            sum = +AmountInFigures(n / 10) + n % 10;
+            return sum;
+        }
 
         //6-задача Цифры числа справа на лево
         public static string PermutationOfDigits(int n)
         {
-            if(n<10)
+            if (n < 10)
             {
-                return n+" ";
+                return n + " ";
             }
-            return n % 10 +" "+ PermutationOfDigits(n / 10);
+            return n % 10 + " " + PermutationOfDigits(n / 10);
         }
 
-        //7-задача
-        //вроде работает -Определение простого числа
-        public static string CheckEase(int n, int k)
+        //7-задач
+        //Определение простого числа
+
+        public static bool CheckEase(int n, int k)
         {
             if (n % 2 != 0)
             {
-               
-                if (n == 1)
+                if (n % k == 0)
                 {
-                    return "Yes";
+                    return false;
                 }
-                if (n >= k && n % k == 0)
-                {
-                    if(n==k)
-                    {
-                        return "Yes";
-                    }
-                    return "No";
-                }
-                else if (n >= k && n % k != 0)
+
+                else if (k < n / 2)
                 {
                     return CheckEase(n, k + 1);
                 }
+                else
+                {
+                    return true;
+                }
             }
+
             else
             {
                 if (n == 2)
                 {
-                    return "Yes";
+                    return true;
                 }
-                return "No";
+                return false;
             }
-           return " ";//не знаю что вернуть в этом случае функции
-    
+         
 
-            }
-
-
-        //8-я задача Разложение на множители
-
-        public static string Factorization(int n, int k)
-        {
-            if (n % 2 != 0)
-            {
-                if (n == 1)
-                {
-                    return n+"";
-                }
-                if (n >= k && n % k == 0)
-                {
-                    if (n == k)
-                    {
-                        return "Yes";
-                    }
-                    return "No";
-                }
-                else if (n >= k && n % k != 0)
-                {
-                    return Factorization(n,k+1);
-                }
-            }
-            else
-            {
-                if (n == 1)
-                {
-                    return n+"";
-                }
-                if (n % 2 != 0)
-                {
-                    return n + "";
-                }
-                return Factorization(n / 2, k);
-            }
-            return " ";//не знаю что вернуть в этом случае функции
-        }
-
-        }
         }
 
 
         
-    
+            public static bool Recursion(int n, int i)
+            {
+                if (n < 2)
+                {
+                    return false;
+                } // Базовый случай 
+                else if (n == 2)
+                {
+                    return true;
+                } // Базовый случай 
+                else if (n % i == 0)
+                {
+                    return false;
+                } // Шаг рекурсии / рекурсивное условие
+                else if (i < n / 2)
+                {
+                    return Recursion(n, i + 1);
+                }
+                else
+                {
+                    return true;
+                }
+
+            }
+
+            //8-я задача Разложение на множители - НЕ работает
+
+            public static string Factorization(int n, int k)
+            {
+                if (n % 2 != 0)
+                {
+                    if (n == 1)
+                    {
+                        return n + "";
+                    }
+                    if (n >= k && n % k == 0)
+                    {
+                        if (n == k)
+                        {
+                            return "Yes";
+                        }
+                        return "No";
+                    }
+                    else if (n >= k && n % k != 0)
+                    {
+                        return Factorization(n, k + 1);
+                    }
+                }
+                else
+                {
+                    if (n == 1)
+                    {
+                        return n + "";
+                    }
+                    if (n % 2 != 0)
+                    {
+                        return n + "";
+                    }
+                    return Factorization(n / 2, k);
+                }
+                return " ";//не знаю что вернуть в этом случае функции
+            }
+
+            public static string Palindrome(string a)
+            {
+                if (a.Length == 1)
+                {
+                    return "Yes";
+                }
+                if (a.Length % 2 == 0)
+                {
+                    return Palindrome(a.Length / 2 + "");
+
+
+                }
+                else
+                {
+                    return "No";
+                }
+                if (a.Length / 2 == 2)
+
+                    return "";
+            }
+
+        }
+    }
+
+
+
+
+
 
 
